41590ba54c79a03eb696ab379380610aa64e3df8
Seungmin-develop <tmdals8798@gmail.com>
2023-09-10 21:58:38 +0900

    add implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.jakewharton.timber:timber:5.0.1")
+    implementation("com.github.bumptech.glide:compose:1.0.0-alpha.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
